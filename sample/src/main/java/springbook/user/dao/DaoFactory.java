004e54b4c471f9afcc7f0ce217e015a41df353c5
wonsik <cws0173@gmail.com>
2019-08-31 20:25:17 +0900

    spring start

--- sample/src/main/java/springbook/user/dao/DaoFactory.java
+++ sample/src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory{
+    @Bean
     public UserDao userDao(){
         UserDao dao = new UserDao(connectionMaker());
         return dao;
     }
-
+    @Bean
     public ConnectionMaker connectionMaker(){
         return new KConnectionMaker();
     }
--- sample/src/main/java/springbook/user/dao/UserDaoTest.java
+++ sample/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -2,10 +2,13 @@ package springbook.user.dao;
 
 import springbook.user.domain.User;
 import java.sql.SQLException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserDaoTest{
     public static void main(String[] args) throws ClassNotFoundException, SQLException{
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao",UserDao.class);
 
         dao.delete(null);
         System.out.println("Delete Success");
