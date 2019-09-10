4e52dc6c7e7cabb45e446000d08a155223e548fd
wonsik <cws0173@gmail.com>
2019-09-10 17:54:39 +0900

    update learning test template
    -make template and callback

--- sample/src/main/java/springbook/learningtest/template/Calculator.java
+++ sample/src/main/java/springbook/learningtest/template/Calculator.java
@@ -6,14 +6,33 @@ import java.io.IOException;
 
 public class Calculator {
 	public Integer calcSum(String filepath)throws IOException{
-		BufferedReader br = new BufferedReader(new FileReader(filepath));
-		Integer sum = 0;
-		String line = null;
-		while((line = br.readLine())!=null) {
-			sum += Integer.valueOf(line);
+		BufferedReaderCallback sumCallBack = new BufferedReaderCallback(){
+			public Integer doSomethingWithReader(BufferedReader br) throws IOException{
+				Integer sum = 0;
+				String line = null;
+				while((line = br.readLine())!=null) {
+					sum+=Integer.valueOf(line);
+				}
+				return sum;
+			}
+		};
+		return fileReadTemplate(filepath, sumCallBack);
+	}
+	
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException{
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			int ret = callback.doSomethingWithReader(br);
+			return ret;
+		}catch(IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		}finally {
+			if(br != null) {
+				try {br.close();}
+				catch(IOException e) {System.out.println(e.getMessage());}
+			}	
 		}
-		
-		br.close();
-		return sum;
 	}
 }
