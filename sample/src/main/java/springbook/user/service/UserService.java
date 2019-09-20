0eaa4b55ebf47d321e45ede835c9ee578623e1d5
wonsik <cws0173@gmail.com>
2019-09-20 20:48:28 +0900

    add add function in userService

--- sample/src/main/java/springbook/user/service/UserService.java
+++ sample/src/main/java/springbook/user/service/UserService.java
@@ -27,4 +27,10 @@ public class UserService {
 				userDao.update(user);
 		}
 	}
+
+	public void add(User user) {
+		if(user.getLevel() == null)
+			user.setLevel(Level.BASIC);
+		userDao.add(user);
+	}
 }
