5d79031555e8c8a73b7033cea94ca8933a4c546e
wonsik <cws0173@gmail.com>
2019-10-05 11:52:08 +0900

    refactoring userService -> pointcut, advice

--- sample/src/main/java/springbook/user/service/UserService.java
+++ sample/src/main/java/springbook/user/service/UserService.java
@@ -1,8 +1,14 @@
 package springbook.user.service;
 
+import java.util.List;
 import springbook.user.domain.User;
 
 public interface UserService {
 	void add(User user);
 	void upgradeLevels();
+	User get(String id);
+	List<User> getAll();
+	void deleteAll();
+	void delete(String id);
+	void update(User user);
 }
--- sample/src/main/java/springbook/user/service/UserServiceImpl.java
+++ sample/src/main/java/springbook/user/service/UserServiceImpl.java
@@ -59,4 +59,29 @@ public class UserServiceImpl implements UserService{
 		
 		this.mailSender.send(mailMessage);
 	}
+
+	@Override
+	public User get(String id) {
+		return userDao.get(id);
+	}
+
+	@Override
+	public List<User> getAll() {
+		return userDao.getAll();
+	}
+
+	@Override
+	public void deleteAll() {
+		userDao.deleteAll();
+	}
+
+	@Override
+	public void delete(String id) {
+		userDao.delete(id);
+	}
+
+	@Override
+	public void update(User user) {
+		userDao.update(user);
+	}
 }
