0280aa3eba4b4e1ba1e3ad75a2eed93cd9829113
wonsik <cws0173@gmail.com>
2019-10-09 15:46:22 +0900

    add DI of file name in XmlSqlService
    - it needs to add dependecy of javax.annotation

--- sample/src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ sample/src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -3,7 +3,7 @@ package springbook.user.sqlservice;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -14,13 +14,15 @@ import springbook.user.sqlservice.jaxb.Sqlmap;
 
 public class XmlSqlService implements SqlService {
 	private Map<String, String> sqlMap = new HashMap<String, String>();
+	private String sqlmapFile;
 	
-	public XmlSqlService() {
+	@PostConstruct
+	public void loadSql() {
 		String contextPath = Sqlmap.class.getPackage().getName();
 		try {			
 			JAXBContext context = JAXBContext.newInstance(contextPath);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+			InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
 			Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
 			for(SqlType sql: sqlmap.getSql()) {
 				sqlMap.put(sql.getKey(), sql.getValue());
@@ -29,6 +31,10 @@ public class XmlSqlService implements SqlService {
 		}catch(JAXBException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	public void setSqlmapFile(String sqlmapFile) {
+		this.sqlmapFile = sqlmapFile;
 	}
 	
 	@Override
