a2ee870e33300f5bc85bd35b8b587b7b385153e5
wonsik <cws0173@gmail.com>
2019-10-09 16:42:26 +0900

    apply strategy pattern and DI
    -  by split SqlService to SqlService, SqlRegistry, and SqlReader
    - and this bean refers itself because it implements 3 interfaces

--- sample/src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ sample/src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -12,12 +12,60 @@ import springbook.user.dao.UserDao;
 import springbook.user.sqlservice.jaxb.SqlType;
 import springbook.user.sqlservice.jaxb.Sqlmap;
 
-public class XmlSqlService implements SqlService {
-	private Map<String, String> sqlMap = new HashMap<String, String>();
-	private String sqlmapFile;
+public class XmlSqlService implements SqlService,SqlRegistry,SqlReader {
+	private SqlReader sqlReader;	//sqlService
+	private SqlRegistry sqlRegistry; //sqlService
+	private Map<String, String> sqlMap = new HashMap<String, String>(); // sqlRegistry
+	private String sqlmapFile;	// sqlReader
+	
+	
+	
+	/******************************************* sqlService implements ****************************************/
+
+	public void setSqlReader(SqlReader sqlReader) {
+		this.sqlReader = sqlReader;
+	}
+	
+	public void setSqlRegistry(SqlRegistry sqlRegistry) {
+		this.sqlRegistry = sqlRegistry;
+	}
 	
 	@PostConstruct
 	public void loadSql() {
+		this.sqlReader.read(this.sqlRegistry);
+	}
+	
+	@Override
+	public String getSql(String key) throws SqlRetrievalFailureException {
+		try {
+			return this.sqlRegistry.findSql(key);
+		}catch(SqlNotFoundException e) {
+			throw new SqlRetrievalFailureException(e);
+		}
+	}
+
+	
+	/******************************************* sqlRegistry implements ****************************************/
+
+	@Override
+	public void registerSql(String key, String sql) {
+		sqlMap.put(key,sql);
+	}
+
+	@Override
+	public String findSql(String key) throws SqlNotFoundException {
+		String sql = sqlMap.get(key);
+		if(sql == null)
+			throw new SqlRetrievalFailureException("can not found the key: "+key);
+		else
+			return sql;
+	}
+
+	
+	/******************************************* sqlReader implements ****************************************/
+
+	@Override
+	public void read(SqlRegistry sqlRegistry) {
 		String contextPath = Sqlmap.class.getPackage().getName();
 		try {			
 			JAXBContext context = JAXBContext.newInstance(contextPath);
@@ -25,7 +73,7 @@ public class XmlSqlService implements SqlService {
 			InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
 			Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
 			for(SqlType sql: sqlmap.getSql()) {
-				sqlMap.put(sql.getKey(), sql.getValue());
+				sqlRegistry.registerSql(sql.getKey(), sql.getValue());
 			}
 			
 		}catch(JAXBException e) {
@@ -35,15 +83,6 @@ public class XmlSqlService implements SqlService {
 	
 	public void setSqlmapFile(String sqlmapFile) {
 		this.sqlmapFile = sqlmapFile;
-	}
-	
-	@Override
-	public String getSql(String key) throws SqlRetrievalFailureException {
-		String sql = sqlMap.get(key);
-		if(sql == null)
-			throw new SqlRetrievalFailureException("can not found the key: "+key);
-		else
-			return sql;
 	}
 
 }
