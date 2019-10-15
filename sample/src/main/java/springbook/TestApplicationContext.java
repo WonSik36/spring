60797641fe2fdfbd4396944948ae410029ef29e3
wonsik <cws0173@gmail.com>
2019-10-15 10:27:12 +0900

    delete beans dataSource, transactionManager in xml

--- sample/src/main/java/springbook/TestApplicationContext.java
+++ sample/src/main/java/springbook/TestApplicationContext.java
@@ -1,9 +1,34 @@
 package springbook;
+import javax.sql.DataSource;
+import com.mysql.jdbc.Driver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
 @ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
+	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost:3306/spring");
+		dataSource.setUsername("root");
+		dataSource.setPassword("tmfl3fkdzk4");
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		DataSourceTransactionManager tm = new DataSourceTransactionManager();
+		tm.setDataSource(dataSource());
+		
+		return tm;
+	}
 	
 }
